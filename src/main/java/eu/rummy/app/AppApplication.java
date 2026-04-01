f9bd8d2bcb4bb37248655b84436ede4068e59ee5
Geo <fierascu@gmail.com>
2026-04-01 07:17:46 +0200

    init

---
+++ src/main/java/eu/rummy/app/AppApplication.java
@@ -0,0 +1,13 @@
+package eu.rummy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/rummy/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.rummy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
